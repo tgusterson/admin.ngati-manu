1d1223005e03d7f29c379e734cd851ff1805ccba
Thom Gusterson <thomgusterson@gmail.com>
2021-02-10 11:00:23 +1300

    Testing alternative netlify identity method

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,55 +1,77 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.com/docs/use-static-query/
- */
+// import React from "react"
+// import PropTypes from "prop-types"
+// import { useStaticQuery, graphql } from "gatsby"
+
+// import Header from "./header"
+// import "./layout.css"
+
+// const Layout = ({ children }) => {
+//   const data = useStaticQuery(graphql`
+//     query SiteTitleQuery {
+//       site {
+//         siteMetadata {
+//           title
+//         }
+//       }
+//     }
+//   `)
+
+//   return (
+//     <>
+//       <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
+//       <div
+//         style={{
+//           margin: `0 auto`,
+//           maxWidth: 960,
+//           padding: `0 1.0875rem 1.45rem`,
+//         }}
+//       >
+//         <main>{children}</main>
+//         <footer
+//           style={{
+//             marginTop: `2rem`,
+//           }}
+//         >
+//           © {new Date().getFullYear()}, Built with
+//           {` `}
+//           <a href="https://www.gatsbyjs.com">Gatsby</a>
+//         </footer>
+//       </div>
+//     </>
+//   )
+// }
+
+// Layout.propTypes = {
+//   children: PropTypes.node.isRequired,
+// }
+
+// export default Layout
 
 import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
-
-import Header from "./header"
-import "./layout.css"
+import IdentityModal, { useIdentityContext } from "react-netlify-identity-widget"
+import "react-netlify-identity-widget/styles.css" // delete if you want to bring your own CSS
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
+  const identity = useIdentityContext()
+  const [dialog, setDialog] = React.useState(false)
+  const name =
+    (identity && identity.user && identity.user.user_metadata && identity.user.user_metadata.name) || "NoName"
 
+  console.log(JSON.stringify(identity))
+  const isLoggedIn = identity && identity.isLoggedIn
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer
-          style={{
-            marginTop: `2rem`,
-          }}
-        >
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
-      </div>
+      <nav style={{ background: "green" }}>
+        {" "}
+        Login Status:
+        <button className="btn" onClick={() => setDialog(true)}>
+          {isLoggedIn ? `Hello ${name}, Log out here!` : "LOG IN"}
+        </button>
+      </nav>
+      <main>{children}</main>
+      <IdentityModal showDialog={dialog} onCloseDialog={() => setDialog(false)} />
     </>
   )
-}
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
 }
 
 export default Layout
--- src/pages/oldindex.js
+++ src/pages/oldindex.js
@@ -1,9 +1,36 @@
-import React from "react"
+import React, { Component } from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+
+// function initNetlifyIdentity() {
+//   console.log("initNetlifyIdentity called.")
+//   const script = document.createElement("script")
+//   script.src = "https://identity.netlify.com/v1/netlify-identity-widget.js"
+//   script.async = true
+//   document.body.appendChild(script)
+// }
+
+// function openNetlifyModal() {
+//   const netlifyIdentity = window.netlifyIdentity
+
+//   if (netlifyIdentity)
+//     netlifyIdentity.open()
+//   else
+//     console.log('netlifyIdentity not defined')
+// }
+
+// class NetlifyIdentity extends Component {
+//   componentDidMount() {
+//     initNetlifyIdentity()
+//   }
+
+//   render() {
+//     return (<div></div>)
+//   }
+// }
 
 const IndexPage = () => {
   return (
