9395df8d5e031fd79a5591447ec9bd7b11eb4efb
Thom Gusterson <thomgusterson@gmail.com>
2021-02-10 13:52:23 +1300

    Added netlify functions

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,22 +5,25 @@ import { getPendingUsers, getApprovedUsers } from "../utils/apiRequests"
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 const IndexPage = () => {
-  const [userData, setUserData] = useState('Loading')
-  const [selectedUser, setSelectedUser] = useState(undefined)
-  const [approvedUsers, setApprovedUsers] = useState([])
-  useEffect(() => {
-    const fn = async () => {
-      const pendingUsers = await getPendingUsers()
-      setUserData(pendingUsers);
-      console.log(pendingUsers)
-    }
-    fn()
-  }, [])
+  // const [userData, setUserData] = useState('Loading')
+  // const [selectedUser, setSelectedUser] = useState(undefined)
+  // const [approvedUsers, setApprovedUsers] = useState([])
+  // useEffect(() => {
+  //   const fn = async () => {
+  //     const pendingUsers = await getPendingUsers()
+  //     setUserData(pendingUsers);
+  //     console.log(pendingUsers)
+  //   }
+  //   fn()
+  // }, [])
   return (
     <div>
       <Layout>
         <Container>
-          <h1>Welcome</h1>
+          <h1 onClick={async () => {
+            const pendingUsers = await getPendingUsers()
+            console.log(pendingUsers)
+          }}>Welcome</h1>
         </Container>
       </Layout>
     </div>
