c7c4fae27031ea8cc11987671c0b12215395a383
Thom Gusterson <thomgusterson@gmail.com>
2021-02-10 20:38:36 +1300

    test

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,6 @@
 import React from "react"
 import IdentityModal, { useIdentityContext } from "react-netlify-identity-widget"
 import Container from 'react-bootstrap/Container';
-import Button from 'react-bootstrap/Button';
 import Header from "./header"
 import "react-netlify-identity-widget/styles.css"
 
@@ -9,13 +8,15 @@ const Layout = ({ children }) => {
   const identity = useIdentityContext()
   const [dialog, setDialog] = React.useState(false)
 
-  // console.log(JSON.stringify(identity))
   const isLoggedIn = identity && identity.isLoggedIn
   return (
     <>
+      <IdentityModal
+        showDialog={dialog}
+        onCloseDialog={() => setDialog(false)}
+      />
       <Container>
         <Header setDialog={setDialog} isLoggedIn={isLoggedIn} />
-        <IdentityModal showDialog={dialog} onCloseDialog={() => setDialog(false)} />
         {isLoggedIn ? <main>{children}</main> : <div>You must log in to access this site</div>}
       </Container>
     </>
