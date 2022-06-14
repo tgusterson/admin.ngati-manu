fb0e8daa26526ac8a82191e2baed3d0e743288b2
tgusterson <thomgusterson@gmail.com>
2022-06-14 12:03:18 +1200

    changed pending user message

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,16 @@
 import React from "react"
 import Layout from "../components/layout"
-import 'bootstrap/dist/css/bootstrap.min.css'
+import "bootstrap/dist/css/bootstrap.min.css"
 
-function getCurrentDate(separator = '') {
+function getCurrentDate(separator = "") {
   let newDate = new Date()
-  let date = newDate.getDate();
-  let month = newDate.getMonth() + 1;
-  let year = newDate.getFullYear();
+  let date = newDate.getDate()
+  let month = newDate.getMonth() + 1
+  let year = newDate.getFullYear()
 
-  return `${year}${separator}${month < 10 ? `0${month}` : `${month}`}${separator}${date}`
+  return `${year}${separator}${
+    month < 10 ? `0${month}` : `${month}`
+  }${separator}${date}`
 }
 
 const IndexPage = () => {
@@ -16,21 +18,25 @@ const IndexPage = () => {
     <div>
       <Layout>
         <h1>Admin Portal Home</h1>
-        <h6>{getCurrentDate('-')}</h6>
-        <small><i>(yyyy-mm-dd)</i></small>
-        <br /><br />
+        <h6>{getCurrentDate("-")}</h6>
+        <small>
+          <i>(yyyy-mm-dd)</i>
+        </small>
+        <br />
+        <br />
         <h3>🎉 Live Features:</h3>
         <ul>
           <li>Automatic emails to approved / rejected applicants</li>
           <li>Approved user CSV download</li>
-          <li>Application review tool (review, approve or decline new applications from the Ngati Manu site)</li>
-          <li>Rejected applications management tool (delete rejected applicants from the database or flag them as 'Pending' if you changed your mind)</li>
-        </ul>
-        <br />
-        <h3>📅 Upcoming Features:</h3>
-        <ul>
-          <li>Approved user search</li>
-          <li>Update user information</li>
+          <li>
+            Application review tool (review, approve or decline new applications
+            from the Ngati Manu site)
+          </li>
+          <li>
+            Rejected applications management tool (delete rejected applicants
+            from the database or flag them as 'Pending' if you changed your
+            mind)
+          </li>
         </ul>
       </Layout>
     </div>
--- src/pages/pending-users.js
+++ src/pages/pending-users.js
@@ -2,10 +2,10 @@ import React, { useState } from "react"
 import { CSVLink } from "react-csv"
 import Layout from "../components/layout"
 import PendingUserCard from "../components/PendingUserCard"
-import CardColumns from 'react-bootstrap/CardColumns';
-import Button from 'react-bootstrap/Button'
+import CardColumns from "react-bootstrap/CardColumns"
+import Button from "react-bootstrap/Button"
 import { getUsersByIndex } from "../utils/apiRequests"
-import 'bootstrap/dist/css/bootstrap.min.css'
+import "bootstrap/dist/css/bootstrap.min.css"
 
 const Page = () => {
   const [users, setUsers] = useState([])
@@ -23,39 +23,54 @@ const Page = () => {
     <div>
       <Layout>
         <h1>Pending Applications</h1>
-        <p>Use this page to review pending applications from the Ngati Manu website.</p>
+        <p>
+          Use this page to review pending applications from the Ngati Manu
+          website.
+        </p>
         <ol>
           <li>Start by clicking the 'Get Pending Users' button.</li>
           <li>From there, all of the 'Pending' applications will appear.</li>
-          <li>Selecting 'View Application' on an application card will display the full application details.</li>
-          <li>Within the application details screen, choose to either 'Approve' or 'Reject' the application (or hit 'Close' if you want to decide later).</li>
+          <li>
+            Selecting 'View Application' on an application card will display the
+            full application details.
+          </li>
+          <li>
+            Within the application details screen, choose to either 'Approve' or
+            'Reject' the application (or hit 'Close' if you want to decide
+            later).
+          </li>
         </ol>
-        <Button
-          className="mb-4"
-          onClick={handleClick}>
+        <Button className="mb-4" onClick={handleClick}>
           Get Pending Users
-          </Button>
+        </Button>
         <div>
           {loading && <p>Loading...</p>}
-          {users.length !== 0 &&
+          {users.length !== 0 && (
             <CSVLink
               className="btn btn-primary mt-3 mb-3"
               filename={"pending_users.csv"}
-              data={users.map((user) => {
+              data={users.map(user => {
                 return { id: JSON.stringify(user.id), ...user.data }
-              })}>
+              })}
+            >
               Download File (.csv)
-            </CSVLink>}
+            </CSVLink>
+          )}
           <CardColumns>
-            {
-              (users.length > 0 && !loading)
-              &&
-              users.map((user, index) =>
-                <PendingUserCard userObject={user.data} id={user.id} key={index} handleClick={handleClick} />
-              )
-            }
+            {users.length > 0 &&
+              !loading &&
+              users.map((user, index) => (
+                <PendingUserCard
+                  userObject={user.data}
+                  id={user.id}
+                  key={index}
+                  handleClick={handleClick}
+                />
+              ))}
           </CardColumns>
-          {(users.length === 0 && typeof users === 'object' && !loading) && <p>No pending users.</p>}
+          {/* {users.length === 0 && typeof users === "object" && !loading && (
+            <p>Click 'Get Pending Users' to get started.</p>
+          )} */}
         </div>
       </Layout>
     </div>
